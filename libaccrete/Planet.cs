7687907a20ebf36cadfb766f43507790357be2c4
Dorian Stoll <dorian@tmsp.ga>
2016-06-20 22:57:07 +0200

    Get a dammn bool type, C

--- libaccrete/Planet.cs
+++ libaccrete/Planet.cs
@@ -11,13 +11,13 @@ namespace Accrete
         public Double a;                    /* semi-major axis of the orbit (in AU)*/
         public Double e;                    /* eccentricity of the orbit         */
         public Double mass;                 /* mass (in solar masses)         */
-        public Int32 gas_giant;             /* TRUE if the planet is a gas giant */
+        public Boolean gas_giant;           /* TRUE if the planet is a gas giant */
         public Int32 orbit_zone;            /* the 'zone' of the planet          */
         public Double radius;               /* equatorial radius (in km)         */
         public Double density;              /* density (in g/cc)             */
         public Double orbital_period;       /* length of the local year (days)   */
         public Double day;                  /* length of the local day (hours)   */
-        public Int32 resonant_period;       /* TRUE if in resonant rotation   */
+        public Boolean resonant_period;     /* TRUE if in resonant rotation   */
         public Int32 axial_tilt;            /* units of degrees             */
         public Double escape_velocity;      /* units of cm/sec             */
         public Double surface_accel;        /* units of cm/sec2             */
@@ -26,7 +26,7 @@ namespace Accrete
         public Double molecule_weight;      /* smallest molecular weight retained*/
         public Double volatile_gas_inventory;
         public Double surface_pressure;     /* units of millibars (mb)         */
-        public Int32 greenhouse_effect;     /* runaway greenhouse effect?    */
+        public Boolean greenhouse_effect;   /* runaway greenhouse effect?    */
         public Double boil_point;           /* the boiling point of water (Kelvin)*/
         public Double albedo;               /* albedo of the planet             */
         public Double surface_temp;         /* surface temperature in Kelvin     */
@@ -34,5 +34,6 @@ namespace Accrete
         public Double cloud_cover;          /* fraction of surface covered         */
         public Double ice_cover;            /* fraction of surface covered         */
         public List<Planet> bodies_orbiting;
+        public Planet next_planet;
     }
 }
