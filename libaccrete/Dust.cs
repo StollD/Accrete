ee9aeacfad23c0164f8f5ae5f29d168b99147af5
Dorian Stoll <dorian@tmsp.ga>
2016-06-20 22:56:52 +0200

    Add some dust

---
+++ libaccrete/Dust.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Accrete
+{
+    /// <summary>
+    /// Stores the data for the dust used in the accretation process
+    /// </summary>
+    public class Dust
+    {
+        public Double inner_edge;
+        public Double outer_edge;
+        public Boolean dust_present;
+        public Boolean gas_present;
+        public Dust next_band;
+    }
+}
