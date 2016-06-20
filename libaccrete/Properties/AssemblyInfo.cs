3611cf75dcdc95e887c5db8d26dff0435b27bfd9
Dorian Stoll <dorian@tmsp.ga>
2016-06-20 19:04:59 +0200

    Add the VS project files

---
+++ libaccrete/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("libaccrete")]
+[assembly: AssemblyDescription("A physical simulation of solar system planet formation.")]
+[assembly: AssemblyProduct("libaccrete")]
+[assembly: AssemblyCopyright("Copyright © Joe Nowakowski, Chuck Swiger, Dorian Stoll 2016")]
+[assembly: ComVisible(false)]
+[assembly: Guid("261efccc-4878-4a36-8537-3587e9d0ea66")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
